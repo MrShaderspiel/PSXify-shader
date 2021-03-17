1bc6762bc1dd153bc0b0dd9ee37bae04d319c1ad
MrGlockenspiel <27740978+MrGlockenspiel@users.noreply.github.com>
2021-03-16 21:24:19 -0400

    Fixed thing I tested then forgot to remove

--- shaders/lib/affine_mapping_emu.glsl
+++ shaders/lib/affine_mapping_emu.glsl
@@ -1,12 +1,5 @@
 #ifdef AFFINE_MAPPING_EMU
 	
-	//gl_Position /= gl_Position.w;
+	gl_Position /= gl_Position.w;
 	
-	// vertex_mv is the vertex in view-space (i.e. viewMatrix * modelMatrix * vertex)
-	
-	vec4 vertex_mv = vertex * gbufferModelView;
-	float dist = length(vertex_mv);
-	affine = dist + ((vertex.w * 8.0) / dist) * 0.5;
-	texcoord = texcoord * affine;
-
 #endif
